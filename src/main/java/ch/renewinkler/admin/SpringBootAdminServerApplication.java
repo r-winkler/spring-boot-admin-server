b0d6fc84c5c86f37ad8bb890d03c047b37a99333
René Winkler <rene.winkler@xovis.com>
2017-04-17 11:22:00 +0200

    first commit

---
+++ src/main/java/ch/renewinkler/admin/SpringBootAdminServerApplication.java
@@ -0,0 +1,14 @@
+package ch.renewinkler.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServerApplication.class, args);
+	}
+}
